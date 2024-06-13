2ab3e9746255d4a50dfcb623ed7eef3aec5de1ba
elliecraun <elliecraun@gmail.com>
2024-06-13 03:11:11 -0500

    Elizabeth Craun theorizes space flight

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,28 @@
 // Declare and assign the variables below
+const input = require('readline-sync');
 
+let spaceShuttleName = "Determination";
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+let milesPerKm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof(spaceShuttleName));
+console.log(typeof(shuttleSpeedMph));
+console.log(typeof(distanceToMarsKm));
+console.log(typeof(distanceToMoonKm));
+console.log(typeof(milesPerKm));
 // Calculate a space mission below
-
+console.log(distanceToMarsKm * milesPerKm);
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
 // Print the results of the space mission calculations below
-
+console.log(spaceShuttleName + " " + "will take" + " " + daysToMars + " " + "days to reach Mars.");
 // Calculate a trip to the moon below
-
+console.log(distanceToMoonKm * milesPerKm)
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(spaceShuttleName + " " + "will take" + " " + daysToMoon + " " + "days to reach the Moon.");
